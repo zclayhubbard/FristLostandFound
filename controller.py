931a85480c58a1cd53dac7b96ca9903bbf84c940
Zyanne <zifc@princeton.edu>
2019-11-29 00:41:30 -0500

    added controller file

---
+++ controller.py
@@ -0,0 +1,18 @@
+#---------------------------------------
+# controller.py
+# Author: Zyanne Clay-Hubbard
+#---------------------------------------
+from sys import argv, exit, stderr
+from flask import Flask, request, make_response, redirect, url_for
+from flask import render_template
+
+app = Flask(__name__, template_folder='.')
+
+
+# -----------------------------------------------------------------------
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print('Usage: ' + argv[0] + ' port')
+        exit(1)
+
+    app.run(host='0.0.0.0', port=int(argv[1]), debug=True)
