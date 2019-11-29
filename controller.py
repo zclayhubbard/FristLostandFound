5b895054881ae98a4e5577c56fb0f016fe50744a
Zyanne <zifc@princeton.edu>
2019-11-29 00:52:30 -0500

    try with buildpack set

--- controller.py
+++ controller.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
         print('Usage: ' + argv[0] + ' port')
         exit(1)
 
-    app.run(host='0.0.0.0', port=int(argv[1]), debug=True)
+    app.run(host='0.0.0.0', port=int(argv[1]), debug=True) 
